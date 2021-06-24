c1a08fc3d107ec4b27fe9575bd8381962293b629
arctic_hen7 <arctic_hen7@pm.me>
2021-06-24 10:18:07 +1000

    refactor: ♻️ broke out serverless handler into separate function

--- graphql_serverless/bin/serverless.rs
+++ graphql_serverless/bin/serverless.rs
@@ -1,14 +1,20 @@
 #![forbid(unsafe_code)]
 
 use netlify_lambda_http::{
-    lambda::{lambda, Context},
+    handler,
+    lambda::{Context, run as run_lambda},
     IntoResponse, Request,
 };
 
 type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
 
-#[lambda(http)]
 #[tokio::main]
-async fn main(_: Request, _: Context) -> Result<impl IntoResponse, Error> {
+async fn main() -> Result<(), Error> {
+    run_lambda(handler(graphql)).await?;
+    Ok(())
+}
+
+// TODO move this into the `lib` crate and import it here
+async fn graphql(_: Request, _: Context) -> Result<impl IntoResponse, Error> {
     Ok("Hello World!")
 }
